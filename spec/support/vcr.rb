8a4dcf650463375174c828b7f59cb4365aa06012
Alex Karpov <alexkarpov878@gmail.com>
2025-05-14 09:59:42 -0700

    Add VCR cassettes for API tests with key filtering

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -6,4 +6,17 @@ VCR.configure do |config|
   config.ignore_localhost = true
   config.configure_rspec_metadata!
   config.default_cassette_options = { record: :new_episodes }
+
+
+  if Rails.application.credentials.weather_api_keys&.dig(:open_weather_map)
+    config.filter_sensitive_data('<OPEN_WEATHER_MAP_API_KEY>') do
+      Rails.application.credentials.weather_api_keys[:open_weather_map]
+    end
+  end
+
+  if Rails.application.credentials.geocoder_api_keys&.dig(:google_api_key)
+    config.filter_sensitive_data('<GOOGLE_GEOCODING_API_KEY>') do
+      Rails.application.credentials.geocoder_api_keys[:google_api_key]
+    end
+  end
 end
